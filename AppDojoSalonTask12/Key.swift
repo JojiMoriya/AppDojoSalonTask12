4b4dd48567a74b8334e0164566698c0c6192eece
Joji Moriya <moriya.joji@gmail.com>
2020-10-30 16:40:48 +0900

    First Commit

---
+++ AppDojoSalonTask12/Key.swift
@@ -0,0 +1,12 @@
+//
+//  Key.swift
+//  AppDojoSalonTask12
+//
+//  Created by 守屋譲司 on 2020/10/30.
+//
+
+import Foundation
+
+enum Key {
+    static let taxKey: String = "defaultTax"
+}
---
+++ AppDojoSalonTask12/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AppDojoSalonTask12
+//
+//  Created by 守屋譲司 on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var priceTextField: UITextField!
+    @IBOutlet private weak var taxTextField: UITextField!
+    @IBOutlet private weak var taxIncludedPriceLabel: UILabel!
+    
+    private let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        taxTextField.text = defaults.string(forKey: Key.taxKey)
+    }
+
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let price = Double(priceTextField.text!) ?? 0
+        let tax = Double(taxTextField.text!) ?? 0
+        let taxIncludedPrice = Int(price * (tax / 100 + 1) )
+        taxIncludedPriceLabel.text = String(taxIncludedPrice)
+        
+        defaults.set(String(tax), forKey: Key.taxKey)
+    }
+}
+
